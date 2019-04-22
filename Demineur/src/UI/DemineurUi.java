be600b4869993629f10a6638efa18d7659887c69
André Vigne <andre.vigne@etu.upmc.fr>
2019-04-22 13:12:09 +0200

    v1
    réécriture de certains morceaux du code

--- Demineur/src/UI/DemineurUi.java
+++ Demineur/src/UI/DemineurUi.java
@@ -29,38 +29,34 @@ public class DemineurUi {
 	private MenuItem nouveau;
 	private MenuItem restart;
 	private boolean premierCoup = true;
+	private Scene scene;
 	
 	
 	public DemineurUi(Stage s) {
 		this.instance = this;
 		this.stage = s;		
 		VBox vbox = new VBox();
-		Scene scene = new Scene(vbox);		
+		this.scene = new Scene(vbox);		
 		this.stage.setScene(scene);
-
 		
 		MenuItem facile = new MenuItem("Facile");
 		
 		HBox hbox = new HBox();
-		MenuBar mb = new MenuBar(initMenuJeu(vbox),initMenuDifficulte(facile));
+		MenuBar mb = new MenuBar(initMenuJeu(vbox),initMenuDifficulte(facile,vbox));
 		mb.setMinSize(140, 25);
 		textBombes = new TextField(String.valueOf(bombes-nbFlags));
-		textBombes.setPrefSize(30, 25);
+		textBombes.setPrefSize(45, 25);
 		textBombes.setFocusTraversable(false);
 		textBombes.setEditable(false);
 		
-		textBombes.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				textBombes.setText(String.valueOf(bombes-nbFlags));
-			}			
-		});
+		textBombes.setOnAction((e) -> 
+		{
+			textBombes.setText(String.valueOf(bombes-nbFlags));
+		});		
 		
 		hbox.getChildren().addAll(mb,textBombes);
 		vbox.getChildren().add(hbox);
-		
-		
-		stage.setTitle("Démineur");
+				
 		stage.setResizable(false);
 		stage.show();
 
@@ -78,14 +74,8 @@ public class DemineurUi {
 			@Override
 			public void handle(ActionEvent event) {
 				premierCoup = true;
-				nbFlags = 0;
-				termine = false;
 				initGrille();
-				setGrille(vbox);
-				vbox.autosize();
-				textBombes.fireEvent(new ActionEvent());
-				stage.setWidth(vbox.getWidth());
-				stage.setHeight(vbox.getHeight());
+				initPartie(vbox);
 			}
 		});
 		
@@ -94,24 +84,34 @@ public class DemineurUi {
 			@Override
 			public void handle(ActionEvent event) {
 				premierCoup = false;
-				nbFlags = 0;
-				termine = false;
 				map.reset();
-				textBombes.fireEvent(new ActionEvent());
-				setGrille(vbox);
+				initPartie(vbox);
 			}
 		});
 		jeu.getItems().addAll(nouveau,restart);
 		return jeu;
 	}
+	
+	private void initPartie(VBox vbox)
+	{
+		stage.setTitle("Démineur");
+		nbFlags = 0;
+		termine = false;
+		setGrille(vbox);
+		vbox.autosize();
+		textBombes.fireEvent(new ActionEvent());
+		stage.setWidth(vbox.getWidth());
+		stage.setHeight(vbox.getHeight());
+	}
 
-	private Menu initMenuDifficulte(MenuItem facile)
+	private Menu initMenuDifficulte(MenuItem facile,VBox vbox)
 	{
 		Menu diff = new Menu("Difficulté");		
 		
 		
 		MenuItem moyen = new MenuItem("Moyen");
 		MenuItem difficile = new MenuItem("Difficile");
+		MenuItem perso = new MenuItem("Personnalisée");
 		
 		facile.setOnAction((e) -> 
 		{
@@ -136,10 +136,131 @@ public class DemineurUi {
 			bombes = 99;
 			nouveau.fire();
 		});
-		diff.getItems().addAll(facile,moyen,difficile);
+		
+		perso.setOnAction((e) -> 
+		{
+			selectionJeuPerso();
+			this.termine = true;
+			this.setGrille(vbox);			
+		});
+		diff.getItems().addAll(facile,moyen,difficile,perso);
 		return diff;
 	}
 	
+	private void selectionJeuPerso() {
+		//Stage stageChoix = new Stage();	
+		VBox vboxChoix = new VBox();
+		Scene sceneChoix = new Scene(vboxChoix);		
+		//stageChoix.setScene(scene);
+		//stageChoix.setTitle("Choix des paramètres");
+		
+		GridPane grid = new GridPane();
+		grid.setHgap(3);
+		grid.setVgap(3);
+	    grid.setPadding(new Insets(5, 5, 5, 5));
+
+	    TextField tW = new TextField("9");
+	    TextField tH = new TextField("9");
+	    TextField tB = new TextField("10");
+	    tW.setMaxSize(45,25);tW.setMinSize(45,25);	    
+	    tH.setMaxSize(45,25);tH.setMinSize(45,25);
+	    tB.setMinSize(45,25);tB.setMaxSize(45,25);
+	    grid.add(tW, 3, 0, 1, 1);
+	    grid.add(tH, 3, 1, 1, 1);
+	    grid.add(tB, 3, 2, 1, 1);
+	    
+	    Slider sW = new Slider();
+	    Slider sH = new Slider();
+	    Slider sB = new Slider();
+	    sW.setMinSize(230, 25);
+	    sH.setMinSize(230, 25);
+	    sB.setMinSize(230, 25);
+	    grid.add(sW, 0, 0, 3, 1);
+	    grid.add(sH, 0, 1, 3, 1);
+	    grid.add(sB, 0, 2, 3, 1);
+	    
+	    sW.setMin(9);
+	    sW.setMax(32);
+	    sW.setMajorTickUnit(1);
+	    sW.setOnMouseDragged((e) ->
+	    {
+	    	tW.setText(String.valueOf((int) (sW.getValue())));
+		    sB.setMax((int) (sH.getValue()* sW.getValue() * 0.85));
+	    });
+	    sH.setMin(9);
+	    sH.setMax(32);
+	    sH.setMajorTickUnit(1);
+	    sH.setOnMouseDragged((e) ->
+	    {
+	    	tH.setText(String.valueOf((int) (sH.getValue())));
+		    sB.setMax((int) (sW.getValue()* sH.getValue() * 0.85));
+	    });
+	    
+	    sB.setMin(10);
+	    sB.setMax(68);
+	    sB.setMajorTickUnit(1);
+	    sB.setOnMouseDragged((e) ->
+	    {
+	    	tB.setText(String.valueOf((int) (sB.getValue())));
+	    });
+	    
+	    tB.setOnKeyTyped((e) -> 
+	    {
+	    	try
+	    	{
+	    		sB.setValue((int) Math.max(Integer.valueOf(tB.getText()),(int) sB.getMin()));
+	    	} catch (NumberFormatException ex) { tB.setText(String.valueOf((int) sB.getMin()));}
+	    });
+	    tW.setOnKeyTyped((e) -> 
+	    {
+	    	try
+	    	{
+	    		sW.setValue((int) Math.max(Integer.valueOf(tW.getText()),(int) sW.getMin()));
+	    	} catch (NumberFormatException ex) { tW.setText(String.valueOf((int) sW.getMin()));}
+	    });
+	    tH.setOnKeyTyped((e) -> 
+	    {
+	    	try
+	    	{
+	    		sH.setValue((int) Math.max(Integer.valueOf(tH.getText()),(int) sH.getMin()));
+	    	} catch (NumberFormatException ex) { tH.setText(String.valueOf((int) sH.getMin()));}
+	    });
+		HBox hboxChoix = new HBox();
+		Button btn = new Button("Valider");
+		Button btn3 = new Button("Annuler");
+		Button btn2 = new Button("Quitter");
+
+		btn.setOnAction((e) -> {
+			taillex = Integer.valueOf(tW.getText());
+			tailley = Integer.valueOf(tH.getText());
+			bombes = Integer.valueOf(tB.getText());
+			stage.setScene(this.scene);
+			nouveau.fire();
+		});
+		
+		btn2.setOnAction((e) -> {
+			this.stage.close();
+		});
+		btn3.setOnAction((e) -> {
+			stage.setScene(this.scene);
+			nouveau.fire();
+		});
+		btn.setMinSize(100, 40);
+		btn2.setMinSize(100, 40);
+		btn3.setMinSize(100, 40);
+		hboxChoix.getChildren().addAll(btn,btn3,btn2);
+		vboxChoix.getChildren().addAll(grid,hboxChoix);
+		this.stage.setScene(sceneChoix);
+		vboxChoix.setMinSize(3 * btn.getMinWidth() + 5, 3* (3+sW.getMinHeight())+ 10 + btn.getMinHeight() +25);
+		
+		stage.setWidth(vboxChoix.getMinWidth());
+		stage.setHeight(vboxChoix.getMinHeight());
+		/*
+		stageChoix.setResizable(false);
+		stageChoix.show();*/
+		
+	}
+
 	private void initGrille()
 	{
 		this.map =  new Grid(taillex,tailley,bombes);
@@ -153,6 +274,7 @@ public class DemineurUi {
 	    grille.setPadding(new Insets(5, 5, 5, 5));
 
 		this.grille.setMinSize(taillex*28+15, (1+tailley)*28+10);
+	    
 		for(int i = 0; i < taillex; i++)
 			for(int j = 0; j < tailley; j++)
 			{
@@ -237,34 +359,36 @@ public class DemineurUi {
 	}
 	
 	private void finPartie(int fin) {
-		Stage stageFin = new Stage();		
+		Stage stageF = new Stage();
 		HBox hboxFin = new HBox();
-		Scene scene = new Scene(hboxFin);		
-		stageFin.setScene(scene);
-		if(fin == 1) stageFin.setTitle("Victoire !");
-		else stageFin.setTitle("Défaite");
+		Scene sceneFin = new Scene(hboxFin);	
+		if(fin == 1) stageF.setTitle("Victoire !");
+		else stageF.setTitle("Défaite");
 		Button btn = new Button("Rejouer");
 		Button btn3 = new Button("Recommencer");
 		Button btn2 = new Button("Quitter");
 
 		btn.setOnAction((e) -> {
+			stage.setScene(this.scene);
 			nouveau.fire();
-			stageFin.close();
+			stageF.close();
 		});
 		
 		btn2.setOnAction((e) -> {
 			this.stage.close();
-			stageFin.close();
+			stageF.close();
 		});
 		btn3.setOnAction((e) -> {
+			stage.setScene(this.scene);
 			restart.fire();
-			stageFin.close();
+			stageF.close();
 		});
-		btn.setPrefSize(100, 40);
-		btn2.setPrefSize(100, 40);
-		btn3.setPrefSize(100, 40);
-		hboxFin.getChildren().addAll(btn,btn3,btn2);
-		stageFin.show();
+		btn.setMinSize(95, 40);
+		btn2.setMinSize(95, 40);
+		btn3.setMinSize(95, 40);
+		hboxFin.getChildren().addAll(btn,btn3,btn2);	
+		stageF.setScene(sceneFin);
+		stageF.show();
 	}
 
 	private int testFinPartie()
